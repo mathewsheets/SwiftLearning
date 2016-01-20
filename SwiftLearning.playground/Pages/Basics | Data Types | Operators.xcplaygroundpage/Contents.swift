95b05044387a18d8dfda478a441a73355a5afea9
Mathew Sheets <mathew.sheets@gmail.com>
2016-01-20 05:54:31 -0500

    Exercise #1

--- SwiftLearning.playground/Pages/Basics | Data Types | Operators.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Basics | Data Types | Operators.xcplaygroundpage/Contents.swift
@@ -447,6 +447,11 @@ print(showHalfOpenRange)
 /*:
 **Exercise:**
 Your a big tipper! When ever you go out to eat you tip 20% of the bill and ever tip less than $3. Create a playground that prints the tip amount and the total about. Within the playground, you should be able to change the bill to see how the tip about varies.
+>> **Example Output:**
+* `What is the bill? $x.xx`
+* `What is the tip percentage? x`
+* `The tip is $x.xx`
+* `The total is $x.xx`
 */
 
 /*:

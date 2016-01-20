07d200a522ab866781f4628850579b6a6c9a6161
Mathew Sheets <mathew.sheets@gmail.com>
2016-01-20 06:07:24 -0500

    Content revision.

--- SwiftLearning.playground/Pages/Basics | Data Types | Operators.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Basics | Data Types | Operators.xcplaygroundpage/Contents.swift
@@ -446,7 +446,7 @@ for halfOpenRangeIndex in 1..<10 {
 print(showHalfOpenRange)
 /*:
 **Exercise:**
-Your a big tipper! When ever you go out to eat you tip 20% of the bill and ever tip less than $3. Create a playground that prints the tip amount and the total about. Within the playground, you should be able to change the bill to see how the tip about varies.
+Your a big tipper! When ever you go out to eat you tip 20% of the bill and never tip less than $3. Create a playground that prints the tip amount and the total amount. Within the playground, you should be able to change the bill to see how the tip amount varies.
 >> **Example Output:**
 * `What is the bill? $x.xx`
 * `What is the tip percentage? x`

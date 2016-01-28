641ebf0ec94e6c07d26a2c8fd6e113d18791ed17
Mathew Sheets <mathew.sheets@gmail.com>
2016-01-27 22:08:47 -0500

    Updates to Strings and Characters: Exercise #2

--- SwiftLearning.playground/Pages/Strings and Characters.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Strings and Characters.xcplaygroundpage/Contents.swift
@@ -164,11 +164,11 @@ print(test)
 /*:
 **Exercise:** Get Acquainted. Get a few properties of each person in your class... name and email are good. Use the `String` and `Character` as well as the data types and operators in [Basics | Data Types | Operators](Basics%20|%20Data%20Types%20|%20Operators) to complete this exercise.
 >> **Example Output:**
-* `My name is Mathew Sheets, my email is swift.dude@gmail.com`
-* `Other students in my class are Annie, Sam, Jack, Hudson, Oliver`
-* `Oliver dropped this class` (**use the string above to pull out the last persons name**)
-* `CARSON was added to the class` (**use the string above and insert another person before the second person**)
-* `Jack and Hudson are in the class` (**use the string above to pull out fourth and fifth person**)
+* `My name is Mathew Sheets, my email is swift.dude@gmail.com.`
+* `Other students in my class are Annie, Sam, Jack, Hudson, Oliver.`
+* `Oliver dropped this class.` (**use the second bullet point string to pull out the last person's name.**)
+* `Other students in my class are Annie, Sam, Jack, Hudson, CARSON was added.`  (**use the second bullet point string and append another person.**)
+* `Jack and Hudson are in the class.` (**use the second bullet point string to pull out third and fourth person.**)
 */
 /*:
 **Checkpoint:**

4f04d4fd90418cf5e383396b4ff23293ef368c0b
Mathew Sheets <mathew.sheets@gmail.com>
2016-02-02 04:52:21 -0500

    Mod the exercise

--- SwiftLearning.playground/Pages/Strings and Characters.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Strings and Characters.xcplaygroundpage/Contents.swift
@@ -162,13 +162,13 @@ let test = abeQuote3.substringWithRange(abeQuote3.startIndex.advancedBy(55)..<ab
 
 print(test)
 /*:
-**Exercise:** Get Acquainted. Get a few properties of each person in your class... name and email are good. Use the `String` and `Character` as well as the data types and operators in [Basics | Data Types | Operators](Basics%20|%20Data%20Types%20|%20Operators) to complete this exercise.
+**Exercise:** Get Acquainted. Get a few properties of each student in your class... name and email are good. Use the `String` and `Character` as well as the data types and operators in [Basics | Data Types | Operators](Basics%20|%20Data%20Types%20|%20Operators) to complete this exercise.
 >> **Example Output:**
 * `My name is Mathew Sheets, my email is swift.dude@gmail.com.`
 * `Other students in my class are Annie, Sam, Jack, Hudson and Oliver.`
-* `Oliver dropped this class.` (**use the second bullet point string to pull out the last person's name.**)
-* `Other students in my class are Annie, Sam, Jack, Hudson, CARSON was added.`  (**use the second bullet point string and append another person.**)
-* `Jack and Hudson are in the class.` (**use the second bullet point string to pull out third and fourth person.**)
+* `Oliver dropped this class.` (**use the second bullet point string to pull out the last students's name.**)
+* `Other students in my class are Annie, Sam, Jack, Hudson and CARSON was added.`  (**use the second bullet point string and append another student.**)
+* `Jack and Hudson are in the class.` (**use the second bullet point string to pull out third and fourth student.**)
 */
 /*:
 **Checkpoint:**

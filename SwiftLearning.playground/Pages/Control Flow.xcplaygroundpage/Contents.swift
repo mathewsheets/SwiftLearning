88d64e9187284c9762f32b472a18f2a575841421
Mathew Sheets <mathew.sheets@gmail.com>
2016-02-03 13:39:23 -0500

    Shift content

--- SwiftLearning.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -343,7 +343,7 @@ if #available(iOS 9, OSX 10.11, *) {
     print("statements to execute when running on lower platforms.")
 }
 /*:
-**Exercise:** You have a secret message to send. Write a playground that can encrypt strings with an alphabetical [caesar cipher](https://en.wikipedia.org/wiki/Caesar_cipher). This cipher can ignore numbers, symbols, and whitespace.
+**Exercise:** You have a secret message to send. Write a playground that can encrypt strings with an alphabetical [caesar cipher](https://en.wikipedia.org/wiki/Caesar_cipher). This cipher can ignore numbers, symbols, and whitespace. Add a shift variable to indicate how many places to shift.
 >> **Example Output:**
 * Decrypted: Nearly all men can stand adversity, but if you want to test a man's character, give him power
 * Encrypted: arneyl nyy zra pna fgnaq nqirefvgl, ohg vs lbh jnag gb grfg n zna'f punenpgre, tvir uvz cbjre

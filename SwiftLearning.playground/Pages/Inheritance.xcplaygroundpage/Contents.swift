2e210d77b9720761631f683ec06e7db93324b046
Mathew Sheets <mathew.sheets@gmail.com>
2016-03-29 21:50:36 -0500

    more content

--- SwiftLearning.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -5,16 +5,16 @@
 # Inheritance
 */
 /*:
- > **Session Overview:**
+ > **Session Overview:** Code reuse is paramount in all programming languages. For languages that support object orientated programming or *OOP*, such as Swift, code inheritance is a technique in sharing and extending code within your programs.
  */
 import Foundation
 /*:
  ## What Is It?
- 
+ When you think of inheritance, you probably think of material wealth that your parents or grandparents pass along to you when they die. Or possibly the genes you inherited from your parents and grandparents. Those aspects also ring true in object oriented programming. In the context of OOP, inheritance means that a class can inherit properties and methods from a superclass. The class that inherited the properties and methods is called the subclass. Swift only has the capability of single inheritance, meaning that a Swift class can only inherit from one superclass. Also, inheritance only applies to reference types, a.k.a classes.
  */
 /*:
  ## The Base Class
- 
+ Classes that don’t inherit from any other class is called the base class. Unlike most OOP languages, Swift classes don’t inherit from a universal base class like *Object*. Classes you define without specifying a superclass automatically become base classes for you do subclass.
  */
 class Father {
 
@@ -46,10 +46,12 @@ let steve = Father(name: "Steve", height: 76, hairColor: "Blond", eyeColor: "Blu
 
 print(steve.sayHello())
 print(steve.description)
-
+/*:
+ 
+ */
 /*:
  ## The Subclass
-
+ Having a class inherit from another class, or subclassing and superclass is done with the colon `:` directly after the class name and then the class you want to inherit after the colon. Once a class is defined as having subclassing another class, the subclass can immediately access the visible properties and methods (type and instance). The subclass can also have new properties and methods.
  */
 class Son: Father {
 
@@ -62,10 +64,12 @@ joe.age = 18
 
 print(joe.sayHello())
 print(joe.description)
-
 /*:
- ## Accessing Superclass Methods, Properties, and Subscripts
  
+ */
+/*:
+ ## Accessing superclass initializers, properties, methods and subscripts
+ The subclass can access the properties and methods from the superclass by using the keyword `super` in front of the property or method you want to access. The `super` keyword can also be used with initializers to delegate initialization to the superclass to initialize stored properties of the superclass.
  */
 class Mother {
     
@@ -116,15 +120,16 @@ class Daughter: Mother {
 let amelia = Daughter(age: 18, name: "Amelia", height: 61, hairColor: "Brown", eyeColor: "Brown")
 
 print(amelia.sayHi())
-
 /*:
- ## Overriding
  
  */
-
+/*:
+ ## Overriding
+ A subclass can change the implementation of a property and method (both type and instance) or a subscript that is inherited from a superclass by *overriding* the implementation inherited. This is done with the `override` keyword in front of the property, method or subscript you want to override.
+ */
 /*:
  ## Overriding Methods
- 
+ Overriding a method (either instance or type) is done by placing the `override` keyword before the function definition. If you override a method that is not defined in the superclass, you will receive a compiler error. When you override a method, it’s the implementation of the subclass that gets executed.
  */
 class Grandson: Son {
 
@@ -137,12 +142,13 @@ class Grandson: Son {
 let grandson = Grandson(name: "Sam", height: 48, hairColor: "Blond", eyeColor: "Green")
 
 print(grandson.sayHello())
-
 /*:
- ## Overriding Properties
  
  */
-
+/*:
+ ## Overriding Properties
+ Overriding a property (either instance or type) is similar to overriding a function; you also place the override keyword before the property definition. It doesn’t matter if the superclass’s property is a stored property or computed property. You can override a property in a subclass as long as the name and type are the same as the superclass. You can override a property’s get and set methods as well as the property observers.
+ */
 class Granddaughter: Daughter {
     
     // Overriding Property Getters and Setters
@@ -183,10 +189,12 @@ granddaughter.height = 5
 granddaughter.age = 6
 
 print(granddaughter.description)
-
+/*:
+ 
+ */
 /*:
  ## Inheritance and Initialization
- 
+ When inheriting properties, especially stored properties, from a superclass, you not only need to initialize store properties within your subclass, you also need to initialize properties in the superclass. You can require an initializer to be present in a subclass with the require keyword preceding the custom initializer. The required custom initializer forces all subclasses to implement the superclass’s required custom initializer.
  */
 class Lightsaber {
     
@@ -209,9 +217,9 @@ class Lightsaber {
         
         return "Hit"
     }
-    
+
     var description: String {
-        
+
         return "Lightsaber"
     }
 }
@@ -219,16 +227,18 @@ class Lightsaber {
 class JediLightsaber: Lightsaber {
 
     required init(color: String) {
-        
+
         super.init(color: color) // delegating to the superclass
     }
-    
+
     override func hitType() -> String {
 
         return "Smack"
     }
 }
-
+/*:
+ 
+ */
 /*:
  ## Preventing Overrides
  
@@ -253,7 +263,9 @@ final class DoubleEdged: SithLightsaber {
 //        return "Bang"
 //    }
 }
-
+/*:
+ 
+ */
 /*:
  ## Type Casting
 
@@ -280,7 +292,9 @@ let sithTrainer = SithTrainerLightsaber(color: "Red")
 var trainers = [Lightsaber]()
 trainers.append(jediTrainer)
 trainers.append(sithTrainer)
-
+/*:
+ 
+ */
 /*:
  ### Checking the type
 
@@ -296,7 +310,9 @@ for trainer in trainers {
         print("Sith \(trainer.color) Trainer Lightsaber")
     }
 }
-
+/*:
+ 
+ */
 /*:
  ### Downcasting to get the type
  
@@ -315,7 +331,9 @@ for trainer in trainers {
         print("\(sithTrainer.hitType()) \(sithTrainer.description)")
     }
 }
-
+/*:
+ 
+ */
 /*:
  ### Type Casting for Any and AnyObject
 
@@ -388,7 +406,9 @@ for anyThing in anyThings {
         print("I'm something strange")
     }
 }
-
+/*:
+ 
+ */
 /*:
  **Exercise:**
  */
@@ -402,11 +422,14 @@ for anyThing in anyThings {
  */
 /*:
  **Keywords to remember:**
- - 'override' = Overriding a property or method of a base class
+ - `super` = To access a superclass's properties, methods or subscripts
+ - `override` = Overriding a property or method of a base class
  - `required` = All subclasses must implement an initializer that is `required`
  - `final` = A class, property or method that cannot be overridden
  - `is` = Type check operator to check if an instance is of a specific type
  - `as` = Type cast operator that will downcast to the subclass type. Use `as?` of casting can fail, use `as!` if downcast will always succeed
+ - `AnyObject` = Represents an instance of any class type
+ - `Any` = Represents an instance of any type, including function types and tuples
  */
 /*:
 **Supporting Chapters:**

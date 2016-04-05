69c1c887f90ae2024f2b54cfee39e3293c43604f
Mathew Sheets <mathew.sheets@gmail.com>
2016-04-05 16:30:23 -0400

    code for protocols

--- SwiftLearning.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -20,8 +20,14 @@ import Foundation
  */
 class Father: Equatable {
     
-    var name: String?
-    var age: Int?
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        
+        self.name = name
+        self.age = age
+    }
 }
 
 func ==(lhs: Father, rhs: Father) -> Bool {
@@ -29,8 +35,27 @@ func ==(lhs: Father, rhs: Father) -> Bool {
     return lhs.name == rhs.name && lhs.age == rhs.age
 }
 
+let moe1 = Father(name: "Moe", age: 28)
+let moe2 = Father(name: "Moe", age: 28)
+let curly = Father(name: "Curly", age: 50)
 
+if moe1 == moe2 {
+    print("same moe")
+} else {
+    print("not the same moe")
+}
 
+if moe1 === moe2 {
+    print("same moe identity")
+} else {
+    print("not the same moe identity")
+}
+
+if moe1 == curly {
+    print("same father")
+} else {
+    print("not the same father")
+}
 /*:
  
  */
@@ -39,7 +64,14 @@ func ==(lhs: Father, rhs: Father) -> Bool {
  */
 class Son: Father, Comparable {
     
-    var favoriteTruck: String?
+    var favoriteTruck: String
+    
+    init(name: String, age: Int, truck: String) {
+
+        favoriteTruck = truck
+        
+        super.init(name: name, age: age)
+    }
 }
 
 func <(lhs: Son, rhs: Son) -> Bool {
@@ -61,6 +93,31 @@ func >(lhs: Son, rhs: Son) -> Bool {
     
     return lhs.age > rhs.age
 }
+
+let larry2 = Son(name: "Larry", age: 2, truck: "Blue Dump")
+let larry4 = Son(name: "Larry", age: 4, truck: "Blue Dump")
+let larry7 = Son(name: "Larry", age: 7, truck: "Blue Dump")
+let larry10 = Son(name: "Larry", age: 10, truck: "Blue Dump")
+
+if larry2 < larry4 {
+    
+    print("larry 2 < larry 4")
+}
+
+if larry4 <= larry4 {
+    
+    print("larry 4 <= larry 4")
+}
+
+if larry7 >= larry4 {
+    
+    print("larry 7 >= larry 4")
+}
+
+if larry10 > larry7 {
+    
+    print("larry 10 >= larry 7")
+}
 /*:
  
  */
@@ -69,13 +126,24 @@ func >(lhs: Son, rhs: Son) -> Bool {
  */
 class Daughter: Father, Hashable {
     
-    var favoriteDoll: String?
+    var favoriteDoll: String
     
     var hashValue: Int {
         
         return "\(name),\(age),\(favoriteDoll)".hashValue
     }
+    
+    init(name: String, age: Int, doll: String) {
+        
+        favoriteDoll = doll
+        
+        super.init(name: name, age: age)
+    }
 }
+
+//saunya
+//abby
+//katie
 /*:
  
  */

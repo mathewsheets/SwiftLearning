d4e63a572d2123f1037ea735364c5e4e58a0552e
Mathew Sheets <mathew.sheets@gmail.com>
2016-04-06 12:18:57 -0400

    Protocols Content

--- SwiftLearning.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@
  */
 import Foundation
 /*:
- ## What Is It?
+ ## What Are They and Why Use Them?
  */
 /*:
  ## Conforming To
@@ -141,16 +141,58 @@ class Daughter: Father, Hashable {
     }
 }
 
-//saunya
-//abby
-//katie
+let abby1 = Daughter(name: "abby", age: 3, doll: "Barbie")
+let abby2 = Daughter(name: "abby", age: 3, doll: "Barbie")
+
+let katie = Daughter(name: "katie", age: 5, doll: "Cabbage Patch")
+
+if abby1.hashValue == abby2.hashValue {
+    print("same hashValue")
+} else {
+    print("different hashValue")
+}
+
+if abby1.hashValue == katie.hashValue {
+    print("same hashValue")
+} else {
+    print("different hashValue")
+}
+
+var fathersByDaughter: [Daughter:Father] = [:]
+
+fathersByDaughter[abby1] = moe1
+
+print(fathersByDaughter[abby1]!.name)
+
+fathersByDaughter[abby2] = curly
+
+print(fathersByDaughter[abby1]!.name)
+
+fathersByDaughter[katie] = curly
+
+if fathersByDaughter[abby2] == fathersByDaughter[katie] {
+    
+    print("abby2 and katie have the same father")
+}
 /*:
  
  */
 /*:
- ## Creating
+ ## Creating Protocols
  
  */
+protocol Crawlable {
+
+    // type & instance properties to conform to
+
+    // type & instance methods to conform to
+}
+
+class Crawler: Crawlable {
+
+}
+
+let crawler = Crawler()
 /*:
  
  */
@@ -158,6 +200,25 @@ class Daughter: Father, Hashable {
  ## Protocols & Properties
  
  */
+protocol Walkable: Crawlable {
+    
+    var direction: String { get set }
+    
+}
+
+class Walker: Walkable {
+    
+    var direction: String
+    
+    init(direction: String) {
+        
+        self.direction = direction
+    }
+}
+
+let walker = Walker(direction: "North")
+
+print(walker.direction)
 /*:
  
  */
@@ -165,6 +226,29 @@ class Daughter: Father, Hashable {
  ## Protocols & Methods
  
  */
+protocol Runnable: Walkable {
+
+    func run(speed: Float)
+}
+
+class Runner: Runnable {
+
+    var direction: String
+
+    init(direction: String) {
+
+        self.direction = direction
+    }
+
+    func run(speed: Float) {
+
+        print("running \(speed) mph \(direction)")
+    }
+}
+
+let runner = Runner(direction: "West")
+
+runner.run(6.5)
 /*:
  
  */
@@ -172,6 +256,28 @@ class Daughter: Father, Hashable {
  ## Protocols & Initializers
  
  */
+protocol Talkable {
+    
+    init(son: Son)
+    
+}
+
+class TalkingSon: Son, Talkable {
+
+    required init(son: Son) {
+
+        super.init(name: son.name, age: son.age, truck: son.favoriteTruck)
+    }
+
+    var description: String {
+
+        return "My name is \(name). I'm \(age) years old and my favorite truck is \(favoriteTruck)"
+    }
+}
+
+let talker = TalkingSon(son: Son(name: "Oliver", age: 2, truck: "Red"))
+
+print(talker.description)
 /*:
  
  */
@@ -179,13 +285,144 @@ class Daughter: Father, Hashable {
  ## Protocols as Types
  
  */
+var crawers: [Crawlable] = []
+
+let walkable: Walkable?
+
+class RunnerWalker: Runnable {
+
+    let walker: Walkable
+
+    var direction: String
+
+    init(walker: Walkable, direction: String) {
+
+        self.walker = walker
+        self.direction = direction
+    }
+
+    func run(speed: Float) {
+
+        print("I can run \(speed) mph")
+    }
+}
 /*:
  
  */
 /*:
- ## Delegating the work
+ ## Delegating the work with Protocols
  
  */
+/*:
+ ### The Protocols
+ */
+protocol DoableAction {
+
+    var action: String { get set }
+}
+
+protocol DoableActionDelegate {
+
+    func willDo(what: DoableAction)
+    func doing(what: DoableAction)
+    func didDo(what: DoableAction)
+}
+
+protocol Doable {
+
+    func doingWhat()
+}
+/*:
+ ### Comforming Types
+ */
+class DoSomething: DoableAction {
+    
+    var doAction: String
+    
+    var action: String {
+        get {
+            return doAction
+        }
+        set {
+            doAction = newValue
+        }
+    }
+    
+    init(doAction: String) {
+        
+        self.doAction = doAction
+    }
+}
+
+class DoSomethingPrinter: DoableActionDelegate {
+
+    func willDo(what: DoableAction) {
+
+        print("will do \(what.action)")
+    }
+
+    func doing(what: DoableAction) {
+
+        print("doing \(what.action)")
+    }
+
+    func didDo(what: DoableAction) {
+
+        print("did do \(what.action)")
+    }
+}
+
+class DoSomethingTimeTracker: DoableActionDelegate {
+    
+    func willDo(what: DoableAction) {
+        
+        print("will track time of doing \(what.action)")
+    }
+    
+    func doing(what: DoableAction) {
+        
+        print("track time of doing \(what.action)")
+    }
+    
+    func didDo(what: DoableAction) {
+        
+        print("did track time of doing \(what.action)")
+    }
+}
+
+class DoingSomething: Doable {
+
+    var action: DoableAction = DoSomething(doAction: "Nothing")
+    var delegate: DoableActionDelegate?
+
+    func doingWhat() {
+
+        delegate?.willDo(action)
+
+        delegate?.doing(action)
+
+        delegate?.didDo(action)
+    }
+}
+/*:
+ ### Bringing it all together
+ */
+let worker = DoingSomething()
+worker.delegate = DoSomethingPrinter() // let's just print what the worker is doing
+worker.doingWhat()
+
+worker.action = DoSomething(doAction: "Eating")
+worker.doingWhat()
+
+worker.action = DoSomething(doAction: "Drinking")
+worker.doingWhat()
+
+worker.action = DoSomething(doAction: "Sleeping")
+worker.doingWhat()
+
+worker.action = DoSomething(doAction: "Busy Work")
+worker.delegate = DoSomethingTimeTracker() // pretend that this will actually persist somewhere
+worker.doingWhat()
 /*:
  
  */
@@ -193,6 +430,30 @@ class Daughter: Father, Hashable {
  ## Type Checking & Type Casting Protocols
  
  */
+var objects: [AnyObject] = []
+
+objects.append(crawler)  // conforms to Crawlable
+objects.append(walker)   // conforms to Walkable
+objects.append(runner)   // conforms to Runnable
+objects.append(talker)   // conforms to Talkable
+objects.append(worker)   // conforms to Doable
+
+for object in objects {
+    switch object {
+    case let crawler as Runner:
+        print("I'm a Runner")
+    case let crawler as Walker:
+        print("I'm a Walker")
+    case let crawler as Crawlable:
+        print("I'm a Crawlable")
+    case let talker as Talkable:
+        print("I'm a Talkable")
+    case let worker as Doable:
+        print("I'm a Doable")
+    default:
+        print("I'm something strange")
+    }
+}
 /*:
  
  */

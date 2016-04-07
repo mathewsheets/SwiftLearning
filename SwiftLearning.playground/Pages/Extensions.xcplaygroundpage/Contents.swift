958e1ec95a109d15d95295a84df84ed81a8e7f79
Mathew Sheets <mathew.sheets@gmail.com>
2016-04-07 15:50:27 -0400

    Content for Extensions

--- SwiftLearning.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -2,10 +2,80 @@
  [Table of Contents](@first) | [Previous](@previous) | [Next](@next)
  - - -
  # Extensions
- * callout(Session Overview):
+ * callout(Session Overview): ADD
  */
 import Foundation
 /*:
+ ## Creating
+ 
+ */
+
+/*:
+ ## Adding New Properties
+ 
+ */
+
+/*:
+ ## Adding New Initializers
+ 
+ */
+
+/*:
+ ## Adding New Methods
+ 
+ */
+
+/*:
+ ## Mutation on value types
+ 
+ */
+
+/*:
+ ## Adding Subscripts
+ 
+ */
+
+/*:
+ ## Adding Nested Types
+ 
+ */
+
+/*:
+ ## Conforming to a Protocol with an Extension
+ 
+ */
+
+/*:
+ ## Protocol Adoption with an Extension
+ 
+ */
+
+/*:
+ ## Protocol Extensions
+ 
+ */
+
+/*:
+ ### Default Implementations for Protocols
+ 
+ */
+
+/*:
+ ### Constraining your Protocol Extension 
+ 
+ */
+
+/*:
+ - - -
+ * callout(Exercise): ADD
+ 
+ **Constraints:**
+ - ADD
+ 
+ * callout(Checkpoint): At this point, ...
+ 
+ **Keywords to remember:**
+ - `extension` = ADD
  * callout(Supporting Materials): Chapters and sections from the Guide and Vidoes from WWDC
  - [Guide: Extensions](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Extensions.html)
  - - -

cc56e3cae02eec54f9a8c2be80f0c02486af03f9
Mathew Sheets <mathew.sheets@gmail.com>
2016-04-11 16:47:59 -0400

    Bottom for JSON & Instances & HTTP & ReST

--- SwiftLearning.playground/Pages/HTTP and ReST.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/HTTP and ReST.xcplaygroundpage/Contents.swift
@@ -6,6 +6,10 @@
  */
 import Foundation
 /*:
+ * callout(Supporting Materials): Additional Resources For Further Reading
+ - [ReST on Wikipedia](https://en.wikipedia.org/wiki/Representational_state_transfer)
+ - [Rest API Tutorial](http://www.restapitutorial.com/)
+ - [NSURLSession](https://developer.apple.com/library/ios/documentation/Foundation/Reference/NSURLSession_class/)
   - - -
  [Table of Contents](@first) | [Previous](@previous) | [Next](@next)
  */
--- SwiftLearning.playground/Pages/JSON and Instances.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/JSON and Instances.xcplaygroundpage/Contents.swift
@@ -6,6 +6,9 @@
  */
 import Foundation
 /*:
+ * callout(Supporting Materials): Additional Resources For Further Reading
+ - [JSON Org](http://www.json.org/)
+ - [NSJSONSerialization](https://developer.apple.com/library/ios/documentation/Foundation/Reference/NSJSONSerialization_Class/index.html)
   - - -
  [Table of Contents](@first) | [Previous](@previous) | [Next](@next)
  */

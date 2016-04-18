ceecaa0e9d476672ee2e41eaa7cc6bb5ac6458c8
Mathew Sheets <mathew.sheets@gmail.com>
2016-04-18 15:35:36 -0400

    Start of Generic Content

--- SwiftLearning.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -5,6 +5,52 @@
  * callout(Session Overview):
  */
 import Foundation
+
+/*:
+ ## Why use Generics
+ 
+ */
+
+/*:
+ ## Using Generics with Functions
+ 
+ */
+
+/*:
+ ## Type Parameters
+ 
+ */
+
+/*:
+ ## Naming Type Parameters
+ 
+ */
+
+/*:
+ ## Generic Types
+ 
+ */
+
+/*:
+ ## Extending Generic Types
+ 
+ */
+
+/*:
+ ## Using Type Constraints
+ 
+ */
+
+/*:
+ ## Associated Types
+ 
+ */
+
+/*:
+ ## The `where` clause
+
+ */
+
 /*:
  * callout(Supporting Materials): Chapters and sections from the Guide and Vidoes from WWDC
  - [Guide: Generics](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Generics.html)

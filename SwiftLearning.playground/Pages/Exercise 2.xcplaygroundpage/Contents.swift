87a3a9037d4e414184b380a10b5d626e95fb6e8a
Mathew Sheets <mathew.sheets@gmail.com>
2017-01-18 20:13:52 -0500

    Content for Exercise 2

--- SwiftLearning.playground/Pages/Exercise 2.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Exercise 2.xcplaygroundpage/Contents.swift
@@ -1,6 +1,18 @@
 /*:
  [Table of Contents](@first) | [Previous](@previous) | [Next](@next)
  - - -
+ * callout(Exercise): Get Acquainted. Get a few properties of each student in your class... name and email are good. Use the `String` and `Character` as well as the data types and operators in [Basics | Data Types | Operators](Basics%20|%20Data%20Types%20|%20Operators) to complete this exercise.
+ 
+ **Example Output:**
+ - `My name is Mathew Sheets, my email is swift.dude@gmail.com.`
+ - `Other students in my class are Annie, Sam, Jack, Hudson and Oliver.`
+ - `Oliver dropped this class.` (**use the second bullet point string to pull out the last students's name.**)
+ - `Other students in my class are Annie, Sam, Jack, Hudson and CARSON was added.`  (**use the second bullet point string and append another student.**)
+ - `Jack and Hudson are in the class.` (**use the second bullet point string to pull out third and fourth student.**)
+ 
+ **Constraints:**
+ - Use a nested tuple to hold your students
+ - Use the string created from the second bullet point for all other string creation
  */
 import Foundation
 

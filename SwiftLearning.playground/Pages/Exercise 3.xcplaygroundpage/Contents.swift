834d632e0045ccd5096fd0770fc540b5c0b3757f
Mathew Sheets <mathew.sheets@gmail.com>
2017-01-18 20:24:54 -0500

    Content for exercise 3

--- SwiftLearning.playground/Pages/Exercise 3.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Exercise 3.xcplaygroundpage/Contents.swift
@@ -1,6 +1,15 @@
 /*:
  [Table of Contents](@first) | [Previous](@previous) | [Next](@next)
  - - -
+ * callout(Exercise): You have a secret message to send. Write a playground that can encrypt strings with an alphabetical [caesar cipher](https://en.wikipedia.org/wiki/Caesar_cipher). This cipher can ignore numbers, symbols, and whitespace.
+ 
+ **Example Output:**
+ - Decrypted: Nearly all men can stand adversity, but if you want to test a man's character, give him power
+ - Encrypted: arneyl nyy zra pna fgnaq nqirefvgl, ohg vs lbh jnag gb grfg n zna'f punenpgre, tvir uvz cbjre
+ 
+ **Constraints:**
+ - The encrypted and decrypted text is case sensitive
+ - Add a shift variable to indicate how many places to shift
  */
 import Foundation
 

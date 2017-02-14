f95bf13c53764e661f75dfd659ce6cde5ef439ec
Mathew Sheets <mathew.sheets@gmail.com>
2017-02-14 05:17:24 -0500

    Swift 3 fixes

--- SwiftLearning.playground/Pages/Exercise 5.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Exercise 5.xcplaygroundpage/Contents.swift
@@ -1,10 +1,15 @@
 /*:
  [Table of Contents](@first) | [Previous](@previous) | [Next](@next)
  - - -
- */
-import Foundation
-
-/*:
+ [Next](@next)
+ * callout(Exercise): Create a playground with pages with each playground page consisting of the previous four exercises. Refactor each exercise leveraging collection types and functions.
+ 
+ **Constraints:**
+ Create a swift file containing your functions for each exercise in the **Sources** directory. Make sure each function has the keyword `public` in front of the keyword `func`.
+ - [Exercise 1](Exercise%201)
+ - [Exercise 2](Exercise%202)
+ - [Exercise 3](Exercise%203)
+ - [Exercise 4](Exercise%204)
  - - -
  [Table of Contents](@first) | [Previous](@previous) | [Next](@next)
  */

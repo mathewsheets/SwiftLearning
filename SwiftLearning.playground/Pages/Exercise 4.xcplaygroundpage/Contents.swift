b5da8a0fea370954f6f2bfbbe95408da9049c169
Mathew Sheets <mathew.sheets@gmail.com>
2017-10-01 14:23:23 -0400

    Fix content

--- SwiftLearning.playground/Pages/Exercise 4.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Exercise 4.xcplaygroundpage/Contents.swift
@@ -4,10 +4,10 @@
  * callout(Exercise): You have to record all the students for your school. Leveraging arrays, dictionaries, and sets, create table like containers for each class. Your classes are Math, Science, English and History with a total of 17 unique students with 7 students in each class. Print out each class roster and experiment with set operations, membership and equality.
  
  **Example Output:**
- - `Math = Mathew Sheets, John Winters, Sam Smith`
- - `Science = Sam Smith, Carson Daily, Adam Aarons`
- - `Union of Math and Science = Mathew Sheets, John Winters, Sam Smith, Carson Daily, Adam Aarons`
- 
+ - `Math = Obi-Wan Kenobi, Padme Amidala, Han Solo, Count Dooku, Boba Fett, Luke Skywalker, Kylo Ren`
+ - `Science = Chew Bacca, Padme Amidala, Poe Dameron, Count Dooku, Lando Calrissian, Darth Vader, Luke Skywalker`
+ - `Union of Math and Science = Darth Vader, Chew Bacca, Obi-Wan Kenobi, Padme Amidala, Poe Dameron, Han Solo, Count Dooku, Lando Calrissian, Boba Fett, Luke Skywalker, Kylo Ren`
+
  **Constraints:**
  - Use Set Operations
      - intersection(_:)
